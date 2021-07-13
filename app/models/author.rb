d51f217501767e56a59b9465a2fe3d38a3a06637
Alan da Silva Alves <linuxfama@gmail.com>
2021-07-13 05:00:39 -0300

    feat: docs: configured data validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :last_name, presence: true
+  validates :description, allow_nil: true, length: { maximum: 500 }
 end
