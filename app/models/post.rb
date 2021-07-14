0174956da37daef122299d412506494c4877ba89
Alan da Silva Alves <linuxfama@gmail.com>
2021-07-14 14:45:58 -0300

    feat: views with rails generate controller posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :author
   validates :title, presence: true, length: { minimum: 3, maximum: 200 }
-  validates :content, allow_nil: true
-  validates :publish_at, allow_nil: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :authors
   get 'site', to: 'site#index'
   root to: 'site#index'
+  get '/posts', to: 'posts#index'
 end
