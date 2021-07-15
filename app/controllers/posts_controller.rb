edb023fdab37550f1de7a21cbc979fe9d8d8f91e
Alan da Silva Alves <linuxfama@gmail.com>
2021-07-14 22:19:38 -0300

    feat:  working partials

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,32 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+
   def index
     @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
